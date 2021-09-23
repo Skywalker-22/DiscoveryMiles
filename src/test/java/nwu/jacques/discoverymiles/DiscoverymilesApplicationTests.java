9720aaf36cbb1ff0f8da89b1a6691f27b4232127
Jacques Myburgh <jmyburgh@netrec.co.za>
2021-09-23 21:30:25 +0200

    Initial

---
+++ src/test/java/nwu/jacques/discoverymiles/DiscoverymilesApplicationTests.java
@@ -0,0 +1,13 @@
+package nwu.jacques.discoverymiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverymilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
