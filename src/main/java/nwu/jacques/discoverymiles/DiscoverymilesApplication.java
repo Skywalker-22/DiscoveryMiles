24a44602b41a72a58da22b479dfce7ae40d48a59
Jacques Myburgh <jmyburgh@netrec.co.za>
2021-09-23 21:50:34 +0200

    boilerplate

--- src/main/java/nwu/jacques/discoverymiles/DiscoverymilesApplication.java
+++ src/main/java/nwu/jacques/discoverymiles/DiscoverymilesApplication.java
@@ -2,9 +2,8 @@ package nwu.jacques.discoverymiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class DiscoverymilesApplication {
 
 	public static void main(String[] args) {
