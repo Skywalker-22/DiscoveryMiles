4b908ce8857c14040031aa86d0d4c3ee8e563b7d
Jacques Myburgh <jmyburgh@netrec.co.za>
2021-09-24 08:50:47 +0200

    feat: added swagger support

--- src/main/java/nwu/jacques/discoverymiles/DiscoverymilesApplication.java
+++ src/main/java/nwu/jacques/discoverymiles/DiscoverymilesApplication.java
@@ -2,14 +2,25 @@ package nwu.jacques.discoverymiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSwagger2
 public class DiscoverymilesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoverymilesApplication.class, args);
+	}
+	@Bean
+	public Docket discoveryMilesApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("nwu.jacques.discoverymiles")).build();
 	}
 
 }
