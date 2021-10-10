1bb9ae1f9d6ad2bfd9619778527b79898c269a72
Jacquesm22 <jacquesmyburgh1999@gmail.com>
2021-10-10 11:38:40 +0200

    feat: created user service

--- src/main/java/nwu/jacques/discoverymiles/controller/UserController.java
+++ src/main/java/nwu/jacques/discoverymiles/controller/UserController.java
@@ -2,7 +2,7 @@ package nwu.jacques.discoverymiles.controller;
 
 import nwu.jacques.discoverymiles.entity.User;
 import nwu.jacques.discoverymiles.exception.ResourceNotFoundException;
-import nwu.jacques.discoverymiles.repository.UserRepository;
+import nwu.jacques.discoverymiles.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,33 +13,27 @@ import java.util.List;
 @RequestMapping("/api")
 public class UserController {
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        return userService.getAllUsers();
     }
 
     @GetMapping("/users/{id}")
     public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long userId) throws ResourceNotFoundException {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found for id : " + userId));
+        User user = userService.getUserById(userId);
         return ResponseEntity.ok().body(user);
     }
 
     @PostMapping("/users")
     public User createUser(@Valid @RequestBody User user) {
-        return userRepository.save(user);
+        return userService.createUser(user);
     }
 
     @PutMapping("/users/{id}")
     public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long userId, @Valid @RequestBody User userDetails) throws ResourceNotFoundException {
-        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found for id : " + userId));
-
-        user.setUsername(userDetails.getUsername());
-        user.setEmail(userDetails.getEmail());
-
-        final User updatedUser = userRepository.save(user);
+        var updatedUser = userService.updateUser(userId, userDetails);
         return ResponseEntity.ok(updatedUser);
     }
 }
